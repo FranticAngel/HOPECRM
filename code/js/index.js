ceab12e24e7306953fbce1115e8173f46bb5d0af
465730396 <465730396@users.noreply.github.com>
2017-08-13 09:51:47 +0800

    首页
    首页

--- code/js/index.js
+++ code/js/index.js
@@ -6,3 +6,10 @@ function gotoProductLifeCyclePage(){
 	$("#sliders").html("");
 	$("#main").load("lifecycle/productLifeCycle.html");
 }
+
+function hideSubMenu(obj){
+	$(obj).next().css("display","none")
+}
+function showSubMenu(obj){
+	$(obj).next().css("display","block")
+}
