c5acc092930be3659a733bc87fc26547e2c576a7
465730396 <465730396@users.noreply.github.com>
2017-08-13 17:47:23 +0800

    李磊
    李磊

--- code/js/login_scripts.js
+++ code/js/login_scripts.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function() {
     $('.page-container form').submit(function(){;
         var username = $(this).find('.username').val();
         var password = $(this).find('.password').val();
+        var password2 = $(this).find('.password2').val();
         if(username == '') {
             $(this).find('.error').fadeOut('fast', function(){
                 $(this).css('top', '27px');
@@ -14,6 +15,15 @@ jQuery(document).ready(function() {
             return false;
         }
         if(password == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '96px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password').focus();
+            });
+            return false;
+        }
+        if(password2 == '') {
             $(this).find('.error').fadeOut('fast', function(){
                 $(this).css('top', '96px');
             });
