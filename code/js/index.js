25719f1c3acbe39748f835c9ba42574f1fae498e
465730396 <465730396@users.noreply.github.com>
2017-08-13 10:05:07 +0800

    会员生命周期
    会员生命周期

--- code/js/index.js
+++ code/js/index.js
@@ -6,6 +6,10 @@ function gotoProductLifeCyclePage(){
 	$("#sliders").html("");
 	$("#main").load("lifecycle/productLifeCycle.html");
 }
+function gotoMemberLifeCyclePage(){
+	$("#sliders").html("");
+	$("#main").load("lifecycle/memberLifeCycle.html");
+}
 
 function hideSubMenu(obj){
 	$(obj).next().css("display","none")
