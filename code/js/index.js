ec31c062ccdb582221651cb41a69b4186ecb4401
luoqiya <30953370+luoqiya@users.noreply.github.com>
2017-08-14 23:26:37 +0800

    3

--- code/js/index.js
+++ code/js/index.js
@@ -4,19 +4,23 @@
 
 function gotoMemberPage(){
 	$("#sliders").load("common/commonEchart.html");
+    $("#course").html("#");
     $("#main").load("client_chart.html");
 }
 
 function gotoUnMemberPage(){
 	$("#sliders").load("common/commonEchart.html");
+    $("#course").html("#");
     $("#main").load("client_chart.html");
 }
 function gotoProductLifeCyclePage(){
 	$("#sliders").html("#");
+    $("#course").html("#");
 	$("#main").load("lifecycle/productLifeCycle.html");
 }
 function gotoMemberLifeCyclePage(){
 	$("#sliders").html("#");
+    $("#course").html("#");
 	$("#main").load("lifecycle/memberLifeCycle.html");
 }
 function gotoWorkPage(){
