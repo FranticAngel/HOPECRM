c1002e2f97fd93421a4f5c06b60fd5f22dddcbea
aheroZ <30947676+aheroZ@users.noreply.github.com>
2017-08-20 21:57:25 +0800

    update
    update

--- code/js/cstinfo.js
+++ code/js/cstinfo.js
@@ -6,4 +6,39 @@ $(function(){
 	$("#edit_info").on("click",function(){
 		
 	})
+	
+	$('.single-baseinfo-record-sort').children().on('click',function(){
+		$(this).parent().children().removeClass();
+		$(this).attr('class','single-baseinfo-record-sort-seleted')
+	})
+	
+	$('.single-baseinfo-selection').children().on('click',function(){
+		$(this).parent().children().removeClass();
+		$(this).attr('class','single-baseinfo-selection-selected')
+	})
 })
+
+
+function   exchange_info_bar(obj){
+	if($(obj).attr('class')=="detail-info-sort-li"){
+		$(obj).attr("class","detail-info-sort-select");
+		if($(obj).prev().length==0){
+			$(obj).next().attr("class","detail-info-sort-li");
+			$("#baseinfo").css("display","");
+			$("#baseinfo-sug").css("display","");
+			$("#cstinfo").css("display","none");
+			$("#cstinfo-sug").css("display","none");
+			$(".single-baseinfo-bar").show();
+		}else{
+			$(obj).prev().attr("class","detail-info-sort-li");
+			$("#baseinfo").css("display","none");
+			$("#baseinfo-sug").css("display","none");
+			
+			$("#cstinfo").css("display","");
+			$("#cstinfo-sug").css("display","");
+			$(".single-baseinfo-bar").hide();
+			
+		}
+	}
+}
+
