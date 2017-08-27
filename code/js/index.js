2c0e20dde9c0c770dc4196801fff7a3f981fffed
luoqiya <30953370+luoqiya@users.noreply.github.com>
2017-08-27 20:10:04 +0800

    12

--- code/js/index.js
+++ code/js/index.js
@@ -34,38 +34,50 @@ function gotoMemberLifeChart(){
 	$("#main").load("lifeValue/lifeChart.html");
 }
 function gotoWorkPage(){
+    $("#course").load("criclework/top/management_top.html");
 	$("#main").load("criclework/management.html");
 }
 
 function gotoContact(){
+    $("#course").load("criclework/top/contact_top.html");
 	$("#main").load("criclework/contact.html");
 }
 
 function gotoEmp(){
+    $("#course").load("criclework/top/emp_top.html");
 	$("#main").load("criclework/employee.html");
 }
 
 function gotoTrends(){
+    $("#course").load("criclework/top/trends_top.html");
 	$("#main").load("criclework/trends.html");
 }
 
 function gotoGuhai(){
+    $("#course").load("criclework/top/conguhai_top.html");
 	$("#main").load("criclework/conguhai.html");
 }
 
 function gotoGhxq(){
+    $("#course").load("criclework/top/ghxq_top.html");
     $("#main").load("criclework/ghxq.html");
 }
 
 function gotoConfig(){
+    $("#course").load("criclework/top/config_top.html");
     $("#main").load("criclework/config.html");
 }
 
+function gotoGrxx(){
+        $("#course").load("criclework/biaotou.html");
+    $("#main").load("criclework/huiyuan.html");
+}
+
 function gotoGeren(){
-    $("#sliders").load("common/commonEchart.html");
-    //$("#sliders").html("");
+    //$("#sliders").load("common/commonEchart.html");
+    $("#sliders").html("");
     $("#course").load("criclework/biaotou.html");
-	$("#main").load("criclework/huiyuan.html");
+    $("#main").load("criclework/huiyuan.html");
 }
 
 function hideSubMenu(obj){
